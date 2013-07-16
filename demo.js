661ee1f96e68f5b6d314b39fcb2c0c6848297287
Ricardo Sismeiro <ricardosismeiro@yahoo.co.uk>
2013-07-17 00:16:39 +0100

    Create demo.js

---
+++ demo.js
@@ -0,0 +1,36 @@
+  
+  var el = document.getElementById('demo');
+  
+  el.addEventListener('tap',function(e){
+      e.target.innerHTML = 'tap';
+  });
+  
+  el.addEventListener('dbltap',function(e){
+      e.target.innerHTML = 'double tap';
+  });
+  
+  el.addEventListener('swipeup',function(e){
+      e.target.innerHTML = 'up';
+  });
+  
+  el.addEventListener('swipedown',function(e){
+      e.target.innerHTML = 'down';
+  });
+  
+  el.addEventListener('swipeleft',function(e){
+      e.target.innerHTML = 'left';
+  });
+  
+  el.addEventListener('swiperigth',function(e){
+      e.target.innerHTML = 'rigth';
+  });
+  
+  el.addEventListener('pinchin',function(e){
+      e.target.innerHTML = 'pinch in '+e.scale;
+  });
+  
+  el.addEventListener('pinchout',function(e){
+      e.target.innerHTML = 'pinch out '+e.scale;
+  });
+  
+  new Touch(el);
